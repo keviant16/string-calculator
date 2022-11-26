6ec41c9377dc1b3287c1f9b0e61cc35d139cd1ce
keviant16 <kevin-jean-charles@outlook.com>
2022-11-26 11:18:38 +0100

    impl add for step one - green

--- src/main/java/com/exalt/company/StringCalculatorKata.java
+++ src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -1,7 +1,21 @@
 package com.exalt.company;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class StringCalculatorKata {
     static int add(String numbers) {
-        return 0;
+        if(numbers.equals("")) return 0;
+        //split to array
+        String[] arrOfNumbers = numbers.split(",");
+
+        //forEach el
+        //parse to int
+        //reduce to sum
+        Optional<Integer> i = Arrays.stream(arrOfNumbers)
+                .map(num ->Integer.parseInt(num))
+                .reduce((num1, num2) -> num1 +num2);
+
+        return i.get();
     }
 }
--- src/test/java/com/exalt/company/StringCalculatorKataTest.java
+++ src/test/java/com/exalt/company/StringCalculatorKataTest.java
@@ -7,10 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringCalculatorKataTest {
 
     @Test
-    void test_step_one(){
+    void test_add_step_one(){
         assertEquals(0, StringCalculatorKata.add(""));
         assertEquals(1, StringCalculatorKata.add("1"));
         assertEquals(3, StringCalculatorKata.add("1,2"));
     }
+
 
 }
