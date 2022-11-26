42fcf0440daea54ab7cc82e1531c9898195159b8
keviant16 <kevin-jean-charles@outlook.com>
2022-11-26 12:07:53 +0100

    add test for step two - red

--- src/test/java/com/exalt/company/StringCalculatorKataTest.java
+++ src/test/java/com/exalt/company/StringCalculatorKataTest.java
@@ -2,6 +2,9 @@ package com.exalt.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+
+import static java.util.stream.Stream.generate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorKataTest {
@@ -20,4 +23,18 @@ public class StringCalculatorKataTest {
     void test_add_numbers_with_comma(){
         assertEquals(3, StringCalculatorKata.add("1,2"));
     }
+
+    @Test
+    void test_add_numbers_with_unknown_amount_of_numbers(){
+        //generate random length
+        int length = (int) (Math.random() * (10 - 1 + 1) + 1);
+
+        //generate string of number with random length
+        String randoNumbers = generate(() -> "1,")
+                .limit(length)
+                .collect(Collectors.joining());
+
+        assertEquals(length, StringCalculatorKata.add(randoNumbers));
+    }
+
 }
