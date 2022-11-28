ab49817e5a740a6385fb64e24f74f013286bb91a
keviant16 <kevin-jean-charles@outlook.com>
2022-11-28 18:55:11 +0100

    refactor add for step 5 - refactor

--- src/main/java/com/exalt/company/StringCalculatorKata.java
+++ src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -1,34 +1,34 @@
 package com.exalt.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+
+import static java.lang.Integer.parseInt;
 
 public class StringCalculatorKata {
     static int add(String numbers) {
 
-        //handle previous and // test
         String delimiter = numbers.startsWith("//") ? String.valueOf(numbers.charAt(2)) : ",";
         String numbersStr = numbers.startsWith("//") ? numbers.substring(4) : numbers;
 
-        //stop condition
         if(numbers.equals("")) return 0;
         if(numbers.startsWith(delimiter) || numbers.contains(",\n") || numbers.contains("\n,")) return -1;
 
-        //split numbers and parse number
-        List<Integer> arrOfNumbers = Arrays.stream(numbersStr.split(delimiter + "|\\s"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        List<String> numbersList = Arrays.asList(numbersStr.split(delimiter + "|\\s"));
+        List<Integer> negativeNumbers = new ArrayList<>();
 
-        //check for negative number
-        List<Integer> negativeList = arrOfNumbers.stream()
-                .filter(n -> n < 0)
-                .collect(Collectors.toList());
+        Integer sum = numbersList.stream()
+                .map(n -> {
+                    Integer integer = parseInt(n);
+                    if(integer < 0) negativeNumbers.add(integer);
+                    return integer;
+                })
+                .reduce(Integer::sum).get();
 
-        if (negativeList.size() > 0) throw new ArithmeticException("negatives not allowed: " + negativeList);
-
-        Integer sum = arrOfNumbers.stream().reduce(Integer::sum).get();
-
+        if (negativeNumbers.size() > 0) throw new ArithmeticException("negatives not allowed: " + negativeNumbers);
         return sum;
     }
 }
